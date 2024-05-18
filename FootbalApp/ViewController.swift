5aa1939b786f854290635a5b97d15c2b2702de48
Tima <158593312+OmurbekovTemirlan@users.noreply.github.com>
2024-05-18 19:39:28 +0600

    Initial Commit

---
+++ FootbalApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FootbalApp
+//
+//  Created by Apple on 18.5.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
